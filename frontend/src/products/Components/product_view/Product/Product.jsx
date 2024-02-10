be023b05a036b5fc32dc743b5e0edb51f0e952fb
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2024-02-10 15:43:59 +0530

    Add image using url

--- frontend/src/products/Components/product_view/Product/Product.jsx
+++ frontend/src/products/Components/product_view/Product/Product.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Product(prop) {
-
   return (
-    <div className='flex gap-4'>
-      <div class="bg-slate-500">
-        <img class="object-scale-down h-24 w-20 " src={prop.path} />
+    <div className="flex gap-4">
+      <div class="bg-slate-500"></div>
+      <div className="flex gap-3">
+        <div
+          className="object-scale-down h-24 w-24 bg-cover bg-center bg-no-repeat rounded-md"
+          style={{
+            backgroundImage: `url(${prop.path})`,
+          }}
+        ></div>
+
+        <p>{prop.Deatils}</p>
       </div>
-      <div>
-        <p>
-          {prop.Deatils}
-        </p>
-      </div>
-
-
-
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
--- frontend/src/products/pages/Products.jsx
+++ frontend/src/products/pages/Products.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import Category from "../Components/Category_view/category";
 
 function Products() {
-  const products = [
-    'Iphone',
-    'Samsung',
-    'Huawei',
-  ]
+  const products = ["Iphone", "Samsung", "Huawei"];
+
   return (
     <div className="flex border-x-cyan-950">
-      {products.map(
-        (product) => {
-          return <Category name={product} />;
-        }
-      )}
+      {products.map((product) => {
+        return <Category name={product} />;
+      })}
     </div>
   );
 }
